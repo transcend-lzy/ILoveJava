42f26f542c240569ae7d83c9c99bc3039915544a
transcend-lzy <60084969+transcend-lzy@users.noreply.github.com>
2021-04-25 19:46:46 +0800

    Update Quene8.java

--- 数据结构/Quene8.java
+++ 数据结构/Quene8.java
@@ -9,30 +9,36 @@ public class Quene8 {
     int[] array = new int[max];
 
     public static void main(String[] args) {
-
+        Quene8 quene8 = new Quene8();
+        quene8.check(0);
     }
-    public void check(int n){
-        if (n == max){
+
+    public void check(int n) {
+        if (n == max) {
             print();
             return;
         }
-        for(int i =0 ;i<n;i++){
+        for (int i = 0; i < max; i++) {
             array[n] = i;
-            if(judge(n)){
-                check(n+1);
+            if (judge(n)) {
+                check(n + 1);
             }
         }
     }
-    public boolean judge(int n){
-        for (int i = 0;i<n;i++){
-            return ((array[i] != array[n])&& Math.abs(n-i)!= Math.abs(array[n] - array[i]));
+
+    public boolean judge(int n) {
+        for (int i = 0; i < n; i++) {
+//            return ((array[i] != array[n])&& Math.abs(n-i)!= Math.abs(array[n] - array[i]));
+            if ((array[i] == array[n]) || Math.abs(n - i) == Math.abs(array[n] - array[i])) {
+                return false;
+            }
         }
         return true;
     }
 
-    private void print(){
-        for(int i = 0; i < max; i++){
-            System.out.print(array[i]+" ");
+    private void print() {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
         System.out.println();
     }

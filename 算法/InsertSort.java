978c5bf0acb29aa249f3cbc4b247b6eddd68ccbd
transcend-lzy <60084969+transcend-lzy@users.noreply.github.com>
2021-05-10 15:58:49 +0800

    插入排序代码更新

--- 算法/InsertSort.java
+++ 算法/InsertSort.java
@@ -9,25 +9,23 @@ import com.sun.jdi.InconsistentDebugInfoException;
 //把 n 个待排序的元素看成为一个有序表和一个无序表，开始时有
 //序表中只包含一个元素，无序表中包含有 n-1 个元素，排序过程中每次从无序表中取出第一个元素，把它的排
 //序码依次与有序表元素的排序码进行比较，将它插入到有序表中的适当位置，使之成为新的有序表。
-public class InsertSort {
+public class ShellSort {
     public static void main(String[] args) {
-
+        int[] arr = {101,34,119,1,-1,89};
     }
-    public void insertSort(int[] array){
-        int temp;
-        int temp2;
-        for (int i = 1; i < array.length; i++) {
-            for(int j =0 ; j < i;j++){
-                if (array[i] < array[j]){
-                    temp = array[i];
-                    array[i] = array[j];
-                    array[i+1] = temp;
-                    for(int index = i+1;index < array.length;index++){
-                        temp2 = array[index+1];
-                        array[index+1] = array[index];
-                    }
-                    break;
-                }
+    public static void InsertSort(int[] arr){
+        int insertVal = 0;
+        int insertIndex = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            insertVal = arr[i];
+            insertIndex = i-1;
+            while (insertIndex >=0 && insertVal < arr[insertIndex]){
+                arr[insertIndex + 1] = arr[insertIndex];
+                insertIndex--;
+            }
+            if (insertIndex + 1 != i){
+                arr[insertIndex + 1] = insertVal;
             }
         }
     }

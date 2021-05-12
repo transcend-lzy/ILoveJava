040366dfc1ea355d16ea334428f8ff88f07b5ca3
transcend-lzy <60084969+transcend-lzy@users.noreply.github.com>
2021-05-12 21:58:39 +0800

    代码更新，删除希尔排序中的插入排序

--- 算法/ShellSort.java
+++ 算法/ShellSort.java
@@ -34,30 +34,6 @@ public class ShellSort {
             }
         }
     }
-
-
-
-
-
-
-
-
-
-    public static void InsertSort(int[] arr){
-        int insertVal = 0;
-        int insertIndex = 0;
-
-        for (int i = 1; i < arr.length; i++) {
-            insertVal = arr[i];
-            insertIndex = i-1;
-            while (insertIndex >=0 && insertVal < arr[insertIndex]){
-                arr[insertIndex + 1] = arr[insertIndex];
-                insertIndex--;
-            }
-            if (insertIndex + 1 != i){
-                arr[insertIndex + 1] = insertVal;
-            }
-        }
-    }
+       
 }
 

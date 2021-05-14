2338dbb4a293d8553d1585a888dec46870abd4d4
transcend-lzy <60084969+transcend-lzy@users.noreply.github.com>
2021-05-14 22:13:18 +0800

    快速排序的代码

--- 算法/QuickSort.java
+++ 算法/QuickSort.java
@@ -1,3 +1,38 @@
-//原理：是对冒泡排序的改进，基本思想是：通过一趟排序将要排序的数据分割成独立的两
-//部分，其中一部分的所有数据都比另外一部分的所有数据都要小，然后再按此方法对这两部分数据分别进行快速排
-//序，整个排序过程可以递归进行，以此达到整个数据变成有序序列
+package com.chaoyue.java;
+
+import javax.xml.stream.events.StartDocument;
+
+/**
+ * @author chaoyue
+ * @data2021-05-14 21:31
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {-9,78,0,23,-567,70, -1,900, 4561};
+    }
+    public static void quickSort(int[] arr,int start,int end) {
+        //把数组中的第0个数字作为标准数
+        if (start < end) {
+            int stard = arr[start];
+
+            int l = start;//一开始是最左边
+            int r = end;//一开始是最右边
+            int temp = 0;
+            //while循环的目的是让比pivot值小的放到左边，大的放到右边
+            while (l < r) {
+                while (l < r && stard <= arr[r]) {
+                    r--;
+                }
+                arr[l] = arr[r];
+                while (l < r && stard >= arr[l]) {
+                    l++;
+                }
+                arr[r] = arr[l];
+
+            }
+            arr[l] = stard;
+            quickSort(arr, start, l);
+            quickSort(arr, l + 1, end);
+        }
+    }
+}

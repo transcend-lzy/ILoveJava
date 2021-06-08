b371eb73a7d69e5afbfcb73394c2acb3801f07df
transcend-lzy <60084969+transcend-lzy@users.noreply.github.com>
2021-06-08 22:23:17 +0800

    赫夫曼树代码更新

--- 数据结构/HuffmanTree.java
+++ 数据结构/HuffmanTree.java
@@ -1,6 +1,59 @@
-//赫夫曼树，一种数据结构，其原理说明如下
-//路径和路径长度：在一棵树中，从一个结点往下可以达到的孩子或孙子结点之间的通路，称为路径。通路中分支的数目称为路径长度。若规定根结点的层数为 1，则从根结点到第 L 层结点的路径长度为 L-1
-//结点的权及带权路径长度：若将树中结点赋给一个有着某种含义的数值，则这个数值称为该结点的权。 结
- //点的带权路径长度为：从根结点到该结点之间的路径长度与该结点的权的乘积
-//赫夫曼树用来编码，求最小的带权路径长度的树结构
-//
+package com.chaoyue.java;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author chaoyue
+ * @data2021-06-08 18:14
+ */
+public class HuffmanTree {
+    public static void main(String[] args) {
+        int arr[] = {13,7,8,3,29,6,1};
+        Node root = creatHuffmanTree(arr);
+        preOrder(root);
+    }
+
+    public static void preOrder(Node root){
+        if(root != null){
+            root.preOrder();
+        }else{
+            System.out.println("空树，不能遍历");
+        }
+    }
+    public static Node creatHuffmanTree(int[] arr){
+        //为操作方便，第一步先进行以下步骤
+        /*
+        1、遍历arr数组
+        2、将arr的每个元素构成一个个Node
+        3、将Node放入到ArrayList中
+         */
+        List<Node> nodes = new ArrayList<>();
+        for (int value: arr){
+            nodes.add(new Node(value));
+        }
+
+        //总体是一个循环的过程，一直循环到，nodes列表当中只剩下最后一个数
+        //因为弄完两个结点就把这两个结点从列表删除出去，然后把他们的合加进来
+        //列表剩的最后一个数就是最后两个结点的合
+        while (nodes.size() > 1){
+            //排序，从小到大
+            Collections.sort(nodes);
+            System.out.println("nodes =" + nodes);
+            //取出根节点权值最小的两颗二叉树
+            Node leftNode = nodes.get(0);//从小到大排序，这里就是0，如果从大到小排序，这里就是最后一个
+            Node rightNode = nodes.get(1);
+            Node parent = new Node(leftNode.value+rightNode.value);
+
+            parent.left = leftNode;
+            parent.right = rightNode;
+            //将构成树的两个结点删除
+            nodes.remove(leftNode);
+            nodes.remove(rightNode);
+            //将他们的合当作新节点的权值，并加入列表
+            nodes.add(parent);
+        }
+        return nodes.get(0);
+    }
+}
